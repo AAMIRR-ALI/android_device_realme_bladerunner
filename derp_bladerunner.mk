733ce2b6c8d81de955d67eacc1b92800e103e852
AAMIRR_ALI <amirwide@gmail.com>
2024-09-20 03:30:55 +0530

    init A15-derp

--- derp_bladerunner.mk
+++ derp_bladerunner.mk
@@ -19,54 +19,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common risingOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Viper
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 #Dolby
 
 $(call inherit-product-if-exists, hardware/dolby/dolby.mk)
-
-#For official Devices:
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := AAMIR_ALI
-MATRIXX_CHIPSET := SM8250
-MATRIXX_BATTERY := 4200mAh
-MATRIXX_DISPLAY := 1080x2400
-
-#EPPE
-TARGET_DISABLE_EPPE := true
-
-#BOOT_ANIMATION
-TARGET_BOOT_ANIMATION_RES := 1440
-
-#AUDIOFX
-TARGET_EXCLUDES_AUDIOFX := true
-
-#Build with Gapps:
-WITH_GMS := true
-
-#Device has UDFPS:
-TARGET_HAS_UDFPS := true
-
-#Blur effect
-TARGET_ENABLE_BLUR := true
-
-#Device has UDFPS:
-TARGET_HAS_UDFPS := true
-
-#Blur effect
-TARGET_ENABLE_BLUR := true
-
-#Exclude Packages
-TARGET_EXCLUDES_AUXIO := true
-TARGET_EXCLUDES_VIA := true
-
 # Inherit from device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_bladerunner
+PRODUCT_NAME := derp_bladerunner
 PRODUCT_DEVICE := bladerunner
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2076
