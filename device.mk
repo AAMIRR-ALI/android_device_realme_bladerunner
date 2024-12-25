b26a24a77b10f3e396aef523fa4e517c656fc74c
AAMIRR_ALI <amirwide@gmail.com>
2024-12-26 02:21:57 +0530

    bladerunner: [TEMP] remove all DisplayCutout

--- device.mk
+++ device.mk
@@ -1,9 +1,5 @@
 #
-<<<<<<< HEAD
-# Copyright (C) 2020 The LineageOS Project
-=======
 # Copyright (C) 2021-2024 The LineageOS Project
->>>>>>> 72e1a19 (lemonadep: displayconfig: Setup density mapping)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
